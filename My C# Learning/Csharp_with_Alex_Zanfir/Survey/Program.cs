2ac2d530701aa72096e51a3163cc5f7c2dd3f2bb
Tee787 <tawhiri.tamihana@gmail.com>
2019-02-26 18:24:09 +1300

    second commit

---
+++ My C# Learning/Csharp_with_Alex_Zanfir/Survey/Program.cs
@@ -0,0 +1,92 @@
+﻿/*In this program, we have designed a survey to ask for basic information from the user.
+ We have also added an "if" statement to notify the user that they haven't entered anything.
+ We had duplicate lines of "Console.WriteLines" inside our if statements that may cause problems if we were to update
+ our program, or add more code in the near future.
+ To rectify this problem we added another "function" outside of our "Main" function. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your firstname? ");
+            var fname = Console.ReadLine();
+            
+            //if (fname == "")        //Our if statements 
+            //{
+            //    //Console.WriteLine("you have not entered anything. Please try again");
+                fname = TryAgain(fname);    //Our function we developed to replace our many console.writeline duplicates
+            //    //fname = Console.ReadLine(); //Used a return in our function to compress our lines of code to one single line of code
+            //}
+
+            Console.WriteLine("What is your surname? ");
+            var lname = Console.ReadLine();
+
+            //if (lname == "")
+            //{
+            //    //Console.WriteLine("you have not entered anything. Please try again");
+                lname = TryAgain(lname);
+            //    //lname = Console.ReadLine();
+            //}
+
+            Console.WriteLine("What is your age? ");
+            var age = Console.ReadLine();
+
+            //if (age == "")
+            //{
+            //    //Console.WriteLine("you have not entered anything. Please try again");
+                age = TryAgain(age);
+            //    //age = Console.ReadLine();
+            //}
+
+            Console.WriteLine("What is your date of birth? ");
+            Console.WriteLine("Day: ");
+            var day = Console.ReadLine();
+            //if (day == "")
+            //{
+            //    //Console.WriteLine("you have not entered anything. Please try again");
+                day = TryAgain(day);
+            //    //day = Console.ReadLine();
+            //}
+            Console.WriteLine("Month: ");
+            var month = Console.ReadLine();
+            //if (month == "")
+            //{
+            //    //Console.WriteLine("you have not entered anything. Please try again");
+                month = TryAgain(month);
+            //    //month = Console.ReadLine();
+            //}
+            
+            Console.WriteLine("Year: ");
+            var year = Console.ReadLine();
+            //if (year == "")
+            //{
+            //    //Console.WriteLine("you have not entered anything. Please try again");
+                year = TryAgain(year);          //What we have done is simplified our code to just one line
+            //    //year = Console.ReadLine();
+            //}
+            Console.WriteLine("Your name is {0} {1}", fname,  lname);
+            Console.WriteLine("Your are {0} years old", age);
+            Console.WriteLine("And your date of birth is {0}/{1}/{2} ", day, month, year);
+            Console.ReadLine();
+            
+
+        }
+
+        static /*void*/string TryAgain(string fname)   //Name of our function to be used in our lines of code
+        {
+            if (fname == "")
+            {
+                Console.WriteLine("you have not entered anything. Please try again");   //Line of code in our function
+                return Console.ReadLine();      //return line of code
+            }
+            return fname;    
+        }
+    }
+}
---
+++ My C# Learning/Csharp_with_Alex_Zanfir/Survey2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your firstname? ");
+            var fname = Console.ReadLine();
+            if (fname == "")
+            {
+                TryAgain();
+                fname = Console.ReadLine();
+            }
+            Console.WriteLine("What is your surname? ");
+            var lname = Console.ReadLine();
+            if (lname == "")
+            {
+                TryAgain();
+                lname = Console.ReadLine();
+            }
+            Console.WriteLine("How old are you? ");
+            var age = Console.ReadLine();
+            if (age == "")
+            {
+                TryAgain();
+                age = Console.ReadLine();
+            }
+            Console.WriteLine("What is your date of birth?");
+            Console.WriteLine("Day: ");
+            var day = Console.ReadLine();
+            if (day == "")
+            {
+                TryAgain();
+                day = Console.ReadLine();
+            }
+            Console.WriteLine("Month: ");
+            var month = Console.ReadLine();
+            if (month == "")
+            {
+                TryAgain();
+                month = Console.ReadLine();
+            }
+            Console.WriteLine("Year: ");
+            var year = Console.ReadLine();
+            if (year == "")
+            {
+                TryAgain();
+                year = Console.ReadLine();
+            }
+
+            Console.WriteLine("Your details are as follows: \nName {0} {1} ", fname, lname);
+            Console.WriteLine("You are {0} years of age ", age);
+            Console.WriteLine("Your date of birth is: {0} / {1} / {2}", day, month, year);
+            Console.ReadLine();
+            if (month == "01")
+            {
+                Console.WriteLine("You are an Aquarius ");
+                month = Console.ReadLine();
+            }
+            else if (month == "02")
+            {
+                Console.WriteLine("You are a Pisces ");
+                month = Console.ReadLine();
+            }
+            else if (month == "03")
+            {
+                Console.WriteLine("You are an Aries ");
+                 = Console.ReadLine();
+            }
+            else if (month == "04")
+            {
+                Console.WriteLine("You are a Taurus");
+                Console.ReadLine();
+            }
+            else if (month == "05")
+            {
+                Console.WriteLine("You are a Gemini");
+                Console.ReadLine();
+            }
+            else if (month == "06")
+            {
+                Console.WriteLine("You are a Cancer");
+                Console.ReadLine();
+            }
+            else if (month == "07")
+            {
+                Console.WriteLine("You are a Leo");
+                Console.ReadLine();
+            }
+            else if (month == "08")
+            {
+                Console.WriteLine("You are a Virgo");
+                Console.ReadLine();
+            }
+            else if (month == "09")
+            {
+                Console.WriteLine("You are a Libra");
+                Console.ReadLine();
+            }
+            else if (month == "10")
+            {
+                Console.WriteLine("You are a Scorpio");
+                Console.ReadLine();
+            }
+            else if (month == "11")
+            {
+                Console.WriteLine("You are a Sagittarius");
+                Console.ReadLine();
+            }
+            else if (month == "12")
+            {
+                Console.WriteLine("You are a Capricorn");
+                Console.ReadLine();
+            }
+        }
+        static void TryAgain()
+        {
+            Console.WriteLine("You haven't entered anything. Please try again:");
+        }
+    }
+}
